3920385c6240efbf404469fd353726a37adbd3b1
AhmedElmanzlawy <ahmed.abdelnaser.elmanzlawy@gmail.com>
2022-02-26 20:17:27 +0200

    edit in order schema and order router

--- models/order.js
+++ models/order.js
@@ -11,7 +11,7 @@ const orderSchema = new mongoose.Schema(
             type:String,
             required:true,
         },
-        customerPhoneNumber:{
+        phoneNumber:{
             type:Number,
             required:true,
         }
