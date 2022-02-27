49415f399a02e9bec05c6e60843dcdeddd53df32
[AhmedkamalR] <abokamal01012@gmail.com>
2022-02-27 22:20:07 +0200

    new edit added

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -3,108 +3,162 @@ const order = require("./../models/order");
 const express = require("express");
 const { validationResult } = require("express-validator");
 
-
-//create order
+//create new order
 exports.createOrders = (request, response, next) => {
-    // let errors = validationResult(request);
-    // if (!errors.isEmpty()) {
-    //     let error = new Error();
-    //     error.status = 422;
-    //     error.message = errors.array().reduce((current, object) => current + object.msg + " ", "")
-    //     throw error;
-    // } 
-      let object = new order({
-
-        customerName:request.body.customerName,
-        customerID:request.body.customerID,
-        phoneNumber:request.body.phoneNumber,
-        shippingAddress:request.body.shippingAddress,
-        orderItems:request.body.orderItems,
-        orderDate:request.body.orderDate,
-        orderStatus:request.body.orderStatus,
-        totalPrice:request.body.totalPrice,
-        shippingPrice:request.body.shippingPrice,
-        paymentType:request.body.paymentType
-        
-      });
-      object
-        .save()
-        .then((data) => {
-          response.status(201).json({ message: "order added", data });
-        })
-        .catch((error) => next(error.message));  
-  };
-
-
-  //getMyOrdersByID //check
-  exports.getMyOrdersByID = (request, response, next) => {
-    // let errors = validationResult(request);
-    // if (!errors.isEmpty()) {
-    //     let error = new Error();
-    //     error.status = 422;
-    //     error.message = errors.array().reduce((current, object) => current + object.msg + " ", "")
-    //     throw error;
-    // } 
-    console.log("here");
-  order.find({customerID:request.params.customerId})
-  .then((data) => {
-    // if (!orderItems && orderItems.length === 0) {
-    //     next(new Error("order not found"));
-    // }
-    response.status(200).json(data);
-  })
-  .catch((error) => {
-    next(error.message);
+  let errors = validationResult(request);
+  if (!errors.isEmpty()) {
+      let error = new Error();
+      error.status = 422;
+      error.message = errors.array().reduce((current, object) => current + object.msg + " ", "")
+      throw error;
+  }
+  let object = new order({
+    customerName: request.body.customerName,
+    customerID: request.body.customerID,
+    phoneNumber: request.body.phoneNumber,
+    shippingAddress: request.body.shippingAddress,
+    orderItems: request.body.orderItems,
+    orderDate: request.body.orderDate,
+    orderStatus: request.body.orderStatus,
+    totalPrice: request.body.totalPrice,
+    shippingPrice: request.body.shippingPrice,
+    paymentType: request.body.paymentType,
   });
+  object
+    .save()
+    .then((data) => {
+      response.status(201).json({ message: "order added", data });
+    })
+    .catch((error) => next(error.message));
+};
+
+//getMyOrdersByID
+exports.getMyOrdersByID = (request, response, next) => {
+  let errors = validationResult(request);
+  if (!errors.isEmpty()) {
+    let error = new Error();
+    error.status = 422;
+    error.message = errors
+      .array()
+      .reduce((current, object) => current + object.msg + " ", "");
+    throw error;
+  }
+  if (request.role == "customer" || request.role == "Admin") {
+    order
+      .find({ customerID: request.params.customerId })
+      .then((data) => {
+        response.status(200).json(data);
+      })
+      .catch((error) => {
+        next(error.message);
+      });
+  } else {
+    throw new Error("Not Authorized. Only customer can access to his order");
+  }
 };
 
 //update orderStatus
 exports.updateOrderStatus = (request, response, next) => {
-    let errors = validationResult(request);
-    if (!errors.isEmpty()) {
-        let error = new Error();
-        error.status = 422;
-        error.message = errors.array().reduce((current, object) => current + object.msg + " ", "")
-        throw error;
-    } 
-    order.findOneAndUpdate(
-      { _id: request.body.id },
-      {
-        $set: {
-            orderStatus: req.body.orderStatus
-        },
-      }
-    )
+  let errors = validationResult(request);
+  if (!errors.isEmpty()) {
+    let error = new Error();
+    error.status = 422;
+    error.message = errors
+      .array()
+      .reduce((current, object) => current + object.msg + " ", "");
+    throw error;
+  }
+  if (request.role == "Admin") {
+    order
+      .findOneAndUpdate(
+        { _id: request.body.id },
+        {
+          $set: {
+            orderStatus: req.body.orderStatus,
+          },
+        }
+      )
       .then((data) => {
-        
         response.status(201).json({ message: "order Status updated", data });
       })
       .catch((error) => {
         next(error);
       });
-  };
+  } else {
+    throw new Error("Not Authorized. Only Admin can do that");
+  }
+};
 
 //get Orders
-exports.getOrders=(request,response,next)=>{
-  //TODO: Add admin authorization
-    order.find({})
-        .then(data=>{
-            response.status(200).json(data)
-        })
-        .catch(error=>{
-            next(error);
-        })
-}  
+exports.getOrders = (request, response, next) => {
+  if (request.role == "Admin") {
+    order
+      .find({})
+      .then((data) => {
+        response.status(200).json(data);
+      })
+      .catch((error) => {
+        next(error);
+      });
+  } else {
+    throw new Error("Not Authorized. Only Admin can do that");
+  }
+};
+
+//update updateOrderToPaid
+exports.updateOrderToPaid = (request, response, next) => {
+  let errors = validationResult(request);
+  if (!errors.isEmpty()) {
+    let error = new Error();
+    error.status = 422;
+    error.message = errors
+      .array()
+      .reduce((current, object) => current + object.msg + " ", "");
+    throw error;
+  }
+  if (request.role == "Admin") {
+    order
+      .findOneAndUpdate(
+        { _id: request.body.id },
+        {
+          $set: {
+            paymentStatus: req.body.paymentStatus,
+            paymentType: req.body.paymentType,
+          },
+        }
+      )
+      .then((data) => {
+        response.status(201).json({ message: "payment Status updated", data });
+      })
+      .catch((error) => {
+        next(error);
+      });
+  } else {
+    throw new Error("Not Authorized. Only Admin can do that");
+  }
+};
+
 
 //delete order
-//   exports.deleteOrder = async (request, response, next) => {
-//         try {
-//             let data = await order.findOneAndDelete({ orderItems: request.body.productId });
-//             if (data == null) throw new Error("order not found");
-//             response.status(200).json({ message: "deleted" })
-//         }
-//         catch (error) {
-//             next(error)
-//         }
-   
-// }
+//   exports.deleteOrder = (request, response, next) => {
+//   let errors = validationResult(request);
+//   if (!errors.isEmpty()) {
+//     let error = new Error();
+//     error.status = 422;
+//     error.message = errors
+//       .array()
+//       .reduce((current, object) => current + object.msg + " ", "");
+//     throw error;
+//   }
+//   if (request.role == "Admin" || request.role == "customer") {
+//     order
+//       .findByIdAndDelete({ orderItems: request.body.orderItems })
+//       .then((data) => {
+//         if (data == null) throw new Error("order Is not Found!");
+//         response.status(200).json({ message: "deleted" });
+//       })
+//       .catch((error) => next(error));
+//   } else {
+//     throw new Error("Not Authorized. Only admin or customer can do that");
+//   }
+// };
