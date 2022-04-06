a4f1528fc4ec25712e9323b59f19072b648ef3b3
[AhmedkamalR] <abokamal01012@gmail.com>
2022-04-06 13:28:38 +0200

    solve conflict

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,6 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 library.add(fab, fas);
-import React from "react";
 
 ReactDOM.render(
   <React.StrictMode>
