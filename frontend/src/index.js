d7e6e30a516c8dabe08b371d3f7e05330d96f9c6
zaki <abdulrahman.zaki.342@gmail.com>
2022-04-06 15:14:41 +0200

    import fontawesome

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "@fortawesome/fontawesome-free/css/all.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { fab } from "@fortawesome/free-brands-svg-icons";
