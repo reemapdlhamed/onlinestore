ec61cf282a080307827a3a7ebdd2693eeeeb9c0b
[AhmedkamalR] <abokamal01012@gmail.com>
2022-04-07 16:42:10 +0200

    Fix some issues

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,5 +1,8 @@
-import { applyMiddleware, createStore } from "redux";
+import {applyMiddleware, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import combineReducer from "./reducer/combineReducer";
+
+
 export const store = createStore(combineReducer, composeWithDevTools(applyMiddleware(thunk)));
+export default store;
