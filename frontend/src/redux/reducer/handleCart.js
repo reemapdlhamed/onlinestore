d28c4dce7d3da85517777e6666394af2dd64e07e
hassanOsama720 <hassan.osama720@gmail.com>
2022-04-07 22:28:16 +0200

    some chonges

--- frontend/src/redux/reducer/handleCart.js
+++ frontend/src/redux/reducer/handleCart.js
@@ -5,11 +5,11 @@ const handleCart =(state = cart, action) => {
     switch (action.type) {
         case "ADDITEM":
             // Check if Product is Already Exist
-            const exist = state.find((x)=> x.id === product.id);
+            const exist = state.find((x)=> x._id === product._id);
             if(exist){
                 // Increase the Quantity
                 return state.map((x)=>
-                x.id === product.id ? {...x, qty: x.qty + 1} : x
+                x._id === product._id ? {...x, qty: x.qty + 1} : x
                 );
             }else{
                 const product = action.payload;
