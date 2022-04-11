b7eb56c8c96c761077f21a575f15bf8d6af31803
[AhmedkamalR] <abokamal01012@gmail.com>
2022-04-11 03:30:22 +0200

    Fix Nav Bar and FAQs Page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
 import '../node_modules/font-awesome/css/font-awesome.min.css'
 import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css'
 import { BrowserRouter } from 'react-router-dom';
