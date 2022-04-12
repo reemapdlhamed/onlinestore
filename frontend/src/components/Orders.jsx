218bd9dc437600b8d02be84492983a5eaf100178
adel essam elfar <adel.essam.elfar@gmail.com>
2022-04-12 15:04:57 +0200

    order status done(packed delievered pending)

--- frontend/src/components/Orders.jsx
+++ frontend/src/components/Orders.jsx
@@ -12,16 +12,35 @@ const Orders = (props) => {
 
   const cartState = useSelector((state) => state.handleCart);
   const orderState = useSelector((state) => state.handleOrders);
-
-  var infoz=[]
+  var orderStatus;
+  var infoz = [];
   for (let i = 0; i < orderState.length; i++) {
     if (orderState[i]._id && id === orderState[i]._id) {
-      
-      infoz.push(orderState[i]);
-      break;
+      orderStatus = orderState[i].orderStatus;
     }
   }
-  console.log("INFOZ",infoz)
+  var packed = "step",
+    shipped = "step",
+    delivered = "step",
+    pending="step";
+    
+  if (orderStatus === "pending") {
+    pending="step completed"
+  } else if (orderStatus === "packed") {
+    packed = "step completed";
+    pending="step completed"
+  } else if (orderStatus === "shipped") {
+    packed = "step completed";
+    pending="step completed"
+    shipped = "step completed";
+  } else if (orderStatus === "delivered") {
+    packed = "step completed";
+    pending="step completed"
+    shipped = "step completed";
+    delivered = "step completed";
+  } else if (orderStatus === "cancelled") {
+  }
+  console.log("pending", pending);
   return (
     <div className="main_container min-vh-100 py-5">
       <div class="container padding-bottom-3x mb-1">
@@ -43,7 +62,7 @@ const Orders = (props) => {
           </div>
           <div class="card-body">
             <div class="steps d-flex flex-wrap flex-sm-nowrap justify-content-between padding-top-2x padding-bottom-1x">
-              <div class="step completed">
+              <div class="step completed" >
                 <div class="step-icon-wrap">
                   <div class="step-icon">
                     <i class="pe-7s-cart"></i>
@@ -51,7 +70,7 @@ const Orders = (props) => {
                 </div>
                 <h4 class="step-title">Confirmed Order</h4>
               </div>
-              <div class="step completed">
+              <div class={pending}>
                 <div class="step-icon-wrap">
                   <div class="step-icon">
                     <i class="pe-7s-config"></i>
@@ -59,7 +78,7 @@ const Orders = (props) => {
                 </div>
                 <h4 class="step-title">Processing Order</h4>
               </div>
-              <div class="step completed">
+              <div class={packed}>
                 <div class="step-icon-wrap">
                   <div class="step-icon">
                     <i class="pe-7s-shopbag"></i>
@@ -67,7 +86,7 @@ const Orders = (props) => {
                 </div>
                 <h4 class="step-title">Packing</h4>
               </div>
-              <div class="step">
+              <div class={shipped}>
                 <div class="step-icon-wrap">
                   <div class="step-icon">
                     <i class="pe-7s-car"></i>
@@ -75,7 +94,7 @@ const Orders = (props) => {
                 </div>
                 <h4 class="step-title">Product Dispatched</h4>
               </div>
-              <div class="step ">
+              <div class={delivered}>
                 <div class="step-icon-wrap">
                   <div class="step-icon">
                     <i class="pe-7s-home"></i>
@@ -99,14 +118,13 @@ const Orders = (props) => {
             </label>
           </div>
           <div class="text-left text-sm-right">
-          <BrowserRouter basename="/calendar"/>
-          <NavLink
-          
-          to={`../order-details/${id}`}
-          className="btn btn-outline-dark  ms-2 px-3 py-2"
-        >
-          view order details
-        </NavLink>
+            <BrowserRouter basename="/calendar" />
+            <NavLink
+              to={`../order-details/${id}`}
+              className="btn btn-outline-dark  ms-2 px-3 py-2"
+            >
+              view order details
+            </NavLink>
           </div>
         </div>
       </div>
