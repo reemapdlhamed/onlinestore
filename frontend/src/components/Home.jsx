20ce8cfb1805c986fd19758c6edf7bd60a08bc25
AhmedElmanzlawy <ahmed.abdelnaser.elmanzlawy@gmail.com>
2022-04-16 15:09:22 +0200

    e

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -56,45 +56,7 @@ const Home = () => {
     className: "center",
     infinite: true,
     centerPadding: "60px",
-    slidesToShow: 5,
-    swipeToSlide: true,
-    afterChange: function(index) {
-      console.log(
-        `Slider Changed to: ${index + 1}, background: #222; color: #bada55`
-      );
-    },
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
-  };
-  const settingsThree = {
-    className: "center",
-    infinite: true,
-    centerPadding: "60px",
-    slidesToShow: 6,
+    slidesToShow: 3,
     swipeToSlide: true,
     afterChange: function(index) {
       console.log(
